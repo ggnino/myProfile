541511d3d2029e32488f1030b387d0be003fce9b
Guillermo Gonzalez <ggnino18@gmail.com>
2023-11-05 12:58:57 -0500

    Cleaned Up The Media Queries

--- src/components/navbar-component/navbar-component.jsx
+++ src/components/navbar-component/navbar-component.jsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from 'react';
-import './navbar-component-styles.scss';
-import img1 from '../../imgs/sun.png';
-import img2 from '../../imgs/full-moon.png';
-import { MyContext } from '../../context';
-import anime from 'animejs';
+import React, { useContext, useEffect } from "react";
+import "./navbar-component-styles.scss";
+import img1 from "../../imgs/sun.png";
+import img2 from "../../imgs/full-moon.png";
+import { MyContext } from "../../context";
+import anime from "animejs";
 
 function Navbar() {
 	// useContext hook for app state
@@ -17,54 +17,54 @@ function Navbar() {
 		if (isMounted && window.screen.width > 1200) {
 			// show navbar-brand
 			anime({
-				targets: '.navbar #navbar-brand',
+				targets: ".navbar #navbar-brand",
 				left: 0,
 			});
 			// show navbar links
-			anime({ targets: '.navbar li', right: 0 });
+			anime({ targets: ".navbar li", right: 0 });
 		}
 		// if border-bottom dark class and light-color class are applied
 		if (
-			myClasses.borderBottom === 'border-bottom' &&
-			myClasses.text === 'light-color'
+			myClasses.borderBottom === "border-bottom" &&
+			myClasses.text === "light-color"
 		) {
 			// set border-bottom light class
 			setMyClasses((c) => {
 				return {
 					...c,
-					borderBottom: 'border-bottom-light',
+					borderBottom: "border-bottom-light",
 				};
 			});
 		}
 		// if border-bottom light class and dark-color class are applied
 		if (
-			myClasses.borderBottom === 'border-bottom-light' &&
-			myClasses.text === 'dark-color'
+			myClasses.borderBottom === "border-bottom-light" &&
+			myClasses.text === "dark-color"
 		) {
 			// border-bottom dark class
 			setMyClasses((c) => {
 				return {
 					...c,
-					borderBottom: 'border-bottom',
+					borderBottom: "border-bottom",
 				};
 			});
 		}
 		// if header is not in view and no border bottom
-		if (!refs.ref && myClasses.borderBottom === '') {
+		if (!refs.ref && myClasses.borderBottom === "") {
 			// set border-bottom
 			setMyClasses((c) => {
 				// if text is light-color
-				if (c.text === 'light-color')
+				if (c.text === "light-color")
 					// set light border
 					return {
 						...c,
-						borderBottom: 'border-bottom-light',
+						borderBottom: "border-bottom-light",
 					};
 				// else set dark border
 				else
 					return {
 						...c,
-						borderBottom: 'border-bottom',
+						borderBottom: "border-bottom",
 					};
 			});
 		}
@@ -74,7 +74,7 @@ function Navbar() {
 			setMyClasses((c) => {
 				return {
 					...c,
-					borderBottom: '',
+					borderBottom: "",
 				};
 			});
 		}
@@ -94,7 +94,7 @@ function Navbar() {
 			<a href="/" id="navbar-brand">
 				Guillermo Gonzalez
 			</a>
-			<ul>
+			<ul className="flex">
 				<li>
 					<img src={img1} alt="light-mode-icon" />
 				</li>
@@ -108,7 +108,7 @@ function Navbar() {
 					<a
 						className={myClasses.text}
 						href="#about"
-						style={refs.ref2 ? { color: '#7fff00' } : { color: '' }}
+						style={refs.ref2 ? { color: "#7fff00" } : { color: "" }}
 					>
 						About
 					</a>
@@ -117,7 +117,7 @@ function Navbar() {
 					<a
 						className={myClasses.text}
 						href="#projects"
-						style={refs.ref3 ? { color: '#7fff00' } : { color: '' }}
+						style={refs.ref3 ? { color: "#7fff00" } : { color: "" }}
 					>
 						Projects
 					</a>
@@ -126,7 +126,7 @@ function Navbar() {
 					<a
 						className={myClasses.text}
 						href="#contact"
-						style={refs.ref4 ? { color: '#7fff00' } : { color: '' }}
+						style={refs.ref4 ? { color: "#7fff00" } : { color: "" }}
 					>
 						Contact
 					</a>
